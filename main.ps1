d7bca27d67453411c274ec08ae588ebbccc48583
mitu217 <mtksk.26884um@gmail.com>
2020-12-19 21:12:44 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,9 @@
+# setup winget
+Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mitu217/dotfiles-windows/main/winget.ps1 -OutFile winget.ps1
+./winget.ps1
+Remove-Item winget.ps1
 
+# when you double-click a zip file, it will be immediately unzipped by 7zip
+Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mitu217/dotfiles-windows/main/7-zip-default-extract.reg -OutFile 7-zip-default-extract.reg
+reg import ./7-zip-default-extract.reg
+Remove-Item ./7-zip-default-extract.reg
