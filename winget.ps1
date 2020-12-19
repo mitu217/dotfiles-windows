8587f216a67d38e0dc55a3c45c0466334e77d004
mitu217 <mtksk.26884um@gmail.com>
2020-12-19 19:56:48 +0900

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,5 +1,5 @@
 # Install winget
-#　Not needed once winget is released
+# Not needed once winget is released
 if (Get-Command winget -errorAction SilentlyContinue)
 {
     "already installed winget"
