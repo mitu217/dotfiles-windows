11109b331e3f597d5e49e17a6e46afd1840ec665
mitu217 <mtksk.26884um@gmail.com>
2020-12-19 23:46:44 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -4,9 +4,9 @@ Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mitu21
 Remove-Item setup_winget.ps1
 
 # setup wsl2
-Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mitu217/dotfiles-windows/main/wsl2.ps1 -OutFile setup_wsl2.ps1
-./setup_wsl2.ps1
-Remove-Item setup_wsl2.ps1
+#Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mitu217/dotfiles-windows/main/wsl2.ps1 -OutFile setup_wsl2.ps1
+#./setup_wsl2.ps1
+#Remove-Item setup_wsl2.ps1
 
 
 # when you double-click a zip file, it will be immediately unzipped by 7zip
