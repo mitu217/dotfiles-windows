fe00d95c60786fb505d35412f858c85829b8ce6b
mitu217 <mtksk.26884um@gmail.com>
2020-12-20 11:15:03 +0900

    Update wsl2.ps1

--- wsl2.ps1
+++ wsl2.ps1
@@ -3,5 +3,12 @@ dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /nores
 
 # enable WSL
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+# update wsl kernel components (https://docs.microsoft.com/ja-jp/windows/wsl/install-win10#step-4---download-the-linux-kernel-update-package)
+Invoke-WebRequest -UseBasicParsing -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile C:\Users\mtksk\Downloads\wsl_update_x64.msi
+msiexec.exe /i "C:\Users\mtksk\Downloads\wsl_update_x64.msi" /quiet
+Remove-Item C:\Users\mtksk\Downloads\wsl_update_x64.msi
+
+# set default version
 wsl --set-default-version 2
 
