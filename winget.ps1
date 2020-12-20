cd8df951f220cd63012b75e81455fd5608efc4bd
mitu217 <mtksk.26884um@gmail.com>
2020-12-20 13:26:34 +0900

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -21,6 +21,20 @@ winget source update
 # install packages
 $WINGET_PACKAGES = @(
     "1password"
+    "CPU-Z"
+    "GPU-Z"
+    "Discord"
+    "Fork"
+    "AutoHotkey"
+    "7Zip"
+    "Windows Terminal"
+    "Unity Hub"
+    "Slack"
+    "Ubuntu"
+    "Visual Studio Code"
+    "Sublime Text"
+    "Google Chrome"
+    "Notion"
 )
 
 foreach($PACKAGE in $WINGET_PACKAGES){
