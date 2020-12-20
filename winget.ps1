facdd4a964c4a8dddd1d175e14c3d74fd2a207ef
mitu217 <mtksk.26884um@gmail.com>
2020-12-20 17:42:47 +0900

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -6,7 +6,7 @@ if (Get-Command winget -errorAction SilentlyContinue)
 }
 else
 {
-    Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/winget-cli/releases/download/v0.2.3162-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile winget-cli.appbundle
+    Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/winget-cli/releases/download/v0.2.2941-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile winget-cli.appbundle
     Add-AppPackage winget-cli.appbundle
     Remove-Item winget-cli.appbundle
 }
