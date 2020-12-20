1ecb999e028416f57a73a1f82128e02bf74f1ee8
mitu217 <mtksk.26884um@gmail.com>
2020-12-20 22:31:48 +0900

    update packages

--- package/scoop/bundle.ps1
+++ package/scoop/bundle.ps1
@@ -1,6 +1,7 @@
 try {
-  Get-Command -Name scoop -ErrorAction Stop
+    Get-Command -Name scoop -ErrorAction Stop
 }
 catch [System.Management.Automation.CommandNotFoundException] {
-  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 }
