d9130ebd142804d04d328da327293b8a660ca695
mitu217 <mtksk.26884um@gmail.com>
2022-10-03 00:14:06 +0900

    Update 01_winget.ps1

--- package/01_winget.ps1
+++ package/01_winget.ps1
@@ -1,8 +1,10 @@
+$WINGET_VERSION = "v1.3.2091"
+
 try {
     Get-Command -Name winget -ErrorAction Stop
 }
 catch [System.Management.Automation.CommandNotFoundException] {
-    Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/winget-cli/releases/download/v0.2.3162-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile winget-cli.appbundle
+    Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/winget-cli/releases/download/${WINGET_VERSION}/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile winget-cli.appbundle
     Add-AppPackage winget-cli.appbundle
     Remove-Item winget-cli.appbundle
 
