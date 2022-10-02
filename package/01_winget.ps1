439c92508865d0a30761c7a3ad4b2ddfe296e0a6
mitu217 <mtksk.26884um@gmail.com>
2022-10-03 00:19:47 +0900

    Update 01_winget.ps1

--- package/01_winget.ps1
+++ package/01_winget.ps1
@@ -4,13 +4,7 @@ try {
     Get-Command -Name winget -ErrorAction Stop
 }
 catch [System.Management.Automation.CommandNotFoundException] {
-    Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/microsoft/winget-cli/releases/download/$WINGET_VERSION/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile winget-cli.appbundle
-    Add-AppPackage winget-cli.appbundle
-    Remove-Item winget-cli.appbundle
-
-    # copy settings
-    # 1. enable experimental mode
-    #   a. list command 
-    #     list command is not perfect (https://github.com/microsoft/winget-cli/issues/119)
-    Copy-Item $DOTFILES/package/winget/settings.json $env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json
+    Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/microsoft/winget-cli/releases/download/$WINGET_VERSION/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile winget-cli.msixbundle
+    Add-AppxPackage winget-cli.msixbundle
+    Remove-Item winget-cli.msixbundle
 }
